2a760d2cc91ef88b3529249d79941d6fdbf3b691
Ayushi-111 <yayushi717@gmail.com>
2021-01-07 15:47:49 +0530

    Welcome to Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
 echo "Welcome to Employee wage Program"
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+		echo "Employee is Present";
+else
+		echo "Employee is Abscent";
+fi
